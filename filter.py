6a2ada17045adf814fe8bd194e9d16b3d9e5f873
Mossy Head <mossy@kali.local>
2023-03-05 16:37:11 +0545

    maps,lambda,filter,args,kwargs

---
+++ filter.py
@@ -0,0 +1,24 @@
+# Filter.py
+
+# filter(function, iterable) takes 2 Arguments
+
+a = [1,2,3,4,5,6,7,8,9]
+
+def is_even(values): # return type should be bool
+    return values%2==0
+    # if values%2==0:
+    #     return True
+    # return False
+
+print(is_even(9))
+result = list(filter(is_even, a))
+print(result)
+
+vowels = ["a", "e", "i", "o", "u"]
+chars = "g","a","b","c","d","e","i","o"
+
+def is_vowel(char):
+    return char in vowels
+
+vowelss = list(filter(is_vowel, chars))
+print(vowelss)
